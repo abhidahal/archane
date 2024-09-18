9b9b54dd41c8bb62b9d3a62e609e4053be24dcd0
abhidahal <87414003+abhidahal@users.noreply.github.com>
2024-09-18 07:00:03 +0545

    refactor(cleanup) : deleted useless files

--- svim/after/plugin/format.lua
+++
@@ -1,2 +0,0 @@
-vim.g.neoformat_try_node_exe = 1
-vim.g.neoformat_enabled_python = { "black" }
--- svim/plugin/nvim-tree.lua
+++
@@ -1,5 +0,0 @@
-require("nvim-tree").setup({
-	actions = { open_file = { quit_on_open = true } },
-	filters = { custom = { "^.git$" } },
-	diagnostics = { enable = true },
-})
